ca10e152e5d634558bb93cfdabdfc250c7df479d
sathu1234 <80046400+sathu1234@users.noreply.github.com>
2021-07-30 20:58:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database();
+    var locn=database.ref("ball/position")
+    locn.on("value",readop)
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x:ball.x+x,y:ball.y+y
+    })
+    }
+
+function readop(data){
+position=data.val();
+ball.x=position.x
+ball.y=position.y
+
+}
